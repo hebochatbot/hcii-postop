7a5f2cdbbf1898ac3841f016da7e1f82d74ecc1e
Katie Williams <kaw1@andrew.cmu.edu>
2018-04-26 15:48:48 -0400

    completed basic prompts in dialogflow and webhook NOTE: wound-is-itchy not completed

--- Chatbot/index.js
+++ Chatbot/index.js
@@ -63,10 +63,9 @@ exports.heboHttp = function heboHttp (req, res) {
       // DIALOG:
       // Can I shower?
       case "can-i-shower":
-        response = "You can bathe; the important thing is to not get the dressing wet. I always recommend a bath over a shower so that you can avoid getting your " + surgeryArea + " wet.";
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
             response = "You can let water run over your " + surgeryArea + ", but you should avoid putting it" +
-                       " directly in a stream of water.";
+                       " directly in a stream of water. I always recommend a bath over a shower so that you can avoid getting your " + surgeryArea + " wet.";
         } else {
             timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
             response = "Please avoid getting the wound and dressing wet for now. Wait until 48 hours after your surgery, " +
@@ -110,178 +109,92 @@ exports.heboHttp = function heboHttp (req, res) {
           response = "Hi there! Hope your recovery is going well! I can answer questions about your surgery that relate to wound care, bleeding, or swelling. You can ask me a question like '" + exampleQuestions[Math.floor(Math.random() * exampleQuestions.length)] + "'";
         }
         break;
-      case "how-can-i-shower":
-        break;
-      case "how-can-i-stop-my-bleeding":
+
+      case "how-can-i-stop-my-bleeding - how":
         // join this together with i am still bleeding and have a variable to track the last time the user said that to see if multiple times in a few hours
+        console.log("visual answer for how to apply pressure");
         break;
+
       case "how-do-i-change-my-dressing":
+        console.log("visual answer for how to change dressing");
         break;
-      case "how-do-i-keep-wound-from-drying-out":
+
+      // DIALOG:
+      // You should put a lot of vaseline. Would you like me to show you how? Yes
+      case "how-do-i-keep-wound-from-drying-out - yes":
+        console.log("visual answer for how to apply vaseline");
         break;
-      case "how-do-i-put-dressing-on-unseen-area":
+
+      // DIALOG:
+      // You should put a lot of vaseline. Would you like me to show you how? Yes
+      case "how-much-vaseline - yes":
+        console.log("visual answer for how to apply vaseline");
         break;
-      case "how-much-vaseline":
+
+      case "how-should-i-apply-pressure":
+        console.log("VISUAL ANSWER for how to apply pressure");
         break;
-      case "how-often-can-i-change-my-dressing":
+
+      // DIALOG:
+      // You should put a lot of vaseline to minimize scarring. Would you like me to show you how? Yes
+      case "how-to-reduce-scarring - yes":
+        console.log("visual answer for how to apply vaseline");
         break;
-      case "how-to-clean-my-wound":
-        break;
-      case "how-to-reduce-scarring":
-        break;
-      case "is-bleeding-normal-right-now":
-        break;
+
       case "is-it-ok-to-bleed-through-dressing":
-        break;
-      case "is-scabbing-normal":
-        break;
-      case "my-wound-is-itchy":
-        break;
-      case "ran-out-of-gauze":
-        break;
-      case "what-if-dressing-gets-wet":
-        break;
-      case "what-if-wound-hurts":
-        break;
-      case "what-level-of-bleeding-is-normal":
-        break;
-      case "what-should-i-do-if-the-dressing-is-falling-off":
-        break;
-      case "when-can-i-change-dressing":
-        break;
-      case "why-cant-i-get-the-wound-wet":
-        break;
-      case "wound-swelling":
-        break;
-      case "wound-swelling-rapidly":
-        break;
-
-
-
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-            response = "You can let water run over your " + surgeryArea + ", but you should avoid putting it" +
-                       " directly in a stream of water.";
+          response = "It is normal to bleed within the first two weeks. If it has been after two weeks, you should apply pressure to the wound and call your doctor if the bleeding hasn't stopped after 30 minutes."
         } else {
-            timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-            response = "Please avoid getting the wound and dressing wet for now. Wait until 48 hours after your surgery, " +
-                        timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString() + ". You can ask me again then for help.";
-        };
+          response = "Bleeding is common in the first 48 hours. You should use a dark towel to apply pressure for 30 minutes. This will help stop the bleeding.";
+        }
         break;
 
-
-
-
-      case "can-i-change-my-dressing":
+      // DIALOG:
+      // What if...?
+      // Is it wet from blood? yes.
+      // has the bleeding stopped? yes.
+      case "What-if-my-dressing-gets-wet? - yes - yes":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-          response = "You should be changing your dressing daily or when it gets wet.";
+          response = "You should change your dressing and clean the wound area.";
         } else {
-          response = "I recommend you wait until " + timeOfSurgery.toDateString() + " at " +
+          response = "Great! It's best to leave your dressing alone. Wait until " + timeOfSurgery.toDateString() + " at " +
                      timeOfSurgery.toLocaleTimeString() + " to change your dressing.";
         }
         break;
-      case "Can-I-change-my-wound-dressing-if-it-is-before-48 hours-but-is-it-bleeding-through-my-dressing?":
-        // TO DO
-        console.log("This is where we show the visual picture for pressure right? halp.")
-        break;
-      case "can-i-shower":
-        response = "You can bathe; the important thing is to not get the dressing wet. We recommend a bath so that you can avoid getting your " + surgeryArea + " wet.";
-        break;
-      case "Do-I need-to-change-my dressing-if-it-is-bleeding-through-it? - no":
+
+      // DIALOG:
+      // What if...?
+      // Is it wet from blood? no.
+      case "What-if-my-dressing-gets-wet? - no":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-          response = "You can go ahead and change your dressing. If your wound is sticking to your dressing, you can try to use a damp cloth to help loosen it.";
+          response = "You should replace your dressing whenever it gets wet.";
         } else {
-          response = "Don't worry about changing your dressing now! You should wait until " + timeOfSurgery.toDateString() + " at " +
-                     timeOfSurgery.toLocaleTimeString() + " to replace your pressure dressing.";
+          response = "It's best to leave your dressing alone. For now you can try to pat it dry. Wait until " + timeOfSurgery.toDateString() + " at " +
+                     timeOfSurgery.toLocaleTimeString() + " to change your dressing.";
         }
         break;
+
+      case "what-should-i-do-if-the-dressing-is-falling-off":
+        if (surgeryArea === "forehead" || surgeryArea === "scalp" || surgeryArea === "back of neck") {
+            response = "I see that your surgery was on your " + surgeryArea + ". If you have hair, it might be getting in the way " +
+                        "of the bandage. Try to secure the tape on the skin under your hair. You can also try wearing a hat " +
+                        "or a scarf to keep the dressing in place.";
+        } else {
+            response = "You can try adding more tape if your dressing is coming loose.";
+        }
+        break;
+
+      case "when-can-i-change-dressing":
+        if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
+          response = "You can change your dressing now. You should be changing your dressing daily or when it gets wet.";
+        } else {
+          response = "You can change your dressing on" + timeOfSurgery.toDateString() + " at " +
+                     timeOfSurgery.toLocaleTimeString() + ".";
+        }
+        break;
+
       default:
         response = "I'm sorry, I don't think I know the answer to that question. I can only answer questions on bleeding, swelling, and wound care."
-
-
-
-
-
-
-
-        case "how-should-i-secure-the-dressing - yes":
-            params = contexts[0].parameters;
-            timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-            response = "This is the example intent. Don't shower until 48 hours after you surgery, which would be " +
-                        timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString();
-            break;
-        case "other-example-intent":
-            timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-            response = "This is the other example intent. Don't shower until 48 hours after you surgery, which would be " +
-                        timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString();
-            break;
-        case "how-often-can-i-change-my-dressing":
-            if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-                response = "You can change your dressing as often as you'd like. We recommend changing it at least once a day.";
-            } else {
-                timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-                response = "You should wait 48 hours after your surgery before changing your dressing, which would be " +
-                            timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString() + ".";
-            };
-            break;
-        case "can-i-change-my-dressing":
-            if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-                response = "Yes, you should change your dressing daily or when it gets wet.";
-            } else {
-                timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-                response = "No, you should wait until " + timeOfSurgery.toDateString() + " at " +
-                           timeOfSurgery.toLocaleTimeString() + " to change your dressing.";
-            };
-            break;
-        case "when-can-i-change-my-dressing":
-            if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-                response = "You should change your dressing once a day or whenever it gets wet.";
-            } else {
-                timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-                response = "You can change your dressing 48 hours after your surgery. In your case, you can change after " +
-                            timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString() + ".";
-            };
-            break;
-        case ("how-should-i-secure-the-dressing"):
-        case ("dressing-keeps-falling-off"):
-            if (surgeryArea === "forehead" || surgeryArea === "scalp" || surgeryArea === "back of neck") {
-                response = "I see that your surgery was on your " + surgeryArea + ". If you have hair, it might be getting in the way " +
-                            "of the bandage. Try to secure the tape on the skin under your hair. You can also try wearing a hate " +
-                            "or a scarf to keep the dressing in place.";
-            } else {
-                response = "You can try adding more tape if your dressing is loose.";
-            }
-            break;
-        case ("can-i-wash-body"):
-            if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-                response = "Yes, but actually since it has been 48 hours after your surgery, you can get your dressing wet if you change it after.";
-            } else {
-                response = "Yes, the important thing is to not get the dressing wet. We recommend a bath so that you can avoid getting your " +
-                surgeryArea + " wet.";
-            };
-            break;
-        case ("how-can-i-shower"):
-            if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-                response = "You can let water run over your " + surgeryArea + ", but you should avoid putting it" +
-                           " directly in a stream of water.";
-            } else {
-                timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-                response = "Please avoid getting the wound and dressing wet for now. Wait until 48 hours after your surgery, " +
-                            timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString() + ". You can ask me again then for help.";
-            };
-            break;
-        case ("what-should-i-do-if-blood-sticks-to-dressing"):
-            if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-                response = "Let the dressing soak in the shower before removing it. When you redress the bandage, you should " +
-                           "increase the amount of vaseline applied and use non-stick pads.";
-            } else {
-                timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-                response = "Please don't try to change the dressing. You should wait until " +
-                            timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString() + " to change your dressing. " +
-                            "Please ask me then if the blood is still sticking to your dressing.";
-            };
-            break;
-        default:
-            response = "I'm sorry, I don't think I know the answer to that question. I can only answer questions on bleeding, swelling, and wound care."
     }
 
 
