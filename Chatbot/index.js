12299899bfa04c798d3ebc87abf785564457e798
daniellehu <daniellh@andrew.cmu.edu>
2018-04-28 13:13:24 -0400

    update index.js to include visual responses

--- Chatbot/index.js
+++ Chatbot/index.js
@@ -20,7 +20,7 @@ const upcomingSurgery = (timeOfSurgery, timeOfNow) => {
 */
 exports.heboHttp = function heboHttp (req, res) {
     console.log(req.body);
-    let response; // the response we'll return
+    let response, speech; // the response we'll return
     let params = req.body.result.parameters;
     const currentTime = new Date(req.body.timestamp);
     const intentName = req.body.result.metadata.intentName;
@@ -36,7 +36,8 @@ exports.heboHttp = function heboHttp (req, res) {
       // Let's check - is it? Yes.
       // You should apply pressure instead. How?
       case "can-i-change-dressing - unsure - yes - how":
-        console.log("visual answer for how to apply pressure");
+        speech = "Here are instructions on how to apply pressure."
+        response = "(APPLY_PRESSURE_RESPONSE)";
         break;
 
       // DIALOG:
@@ -44,7 +45,8 @@ exports.heboHttp = function heboHttp (req, res) {
       // Is it still bleeding? Yes.
       // You should apply pressure instead. How?
       case "can-i-change-dressing - yes - how":
-        console.log("visual answer for how to apply pressure");
+        speech = "Here are instructions on how to apply pressure."
+        response = "(APPLY_PRESSURE_RESPONSE)";
         break;
 
       // DIALOG:
@@ -53,9 +55,9 @@ exports.heboHttp = function heboHttp (req, res) {
       // You should change only if after 48 hours...
       case "can-i-change-dressing - no":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-          response = "You can change your dressing now. You should be changing your dressing daily or when it gets wet.";
+          response = speech = "You can change your dressing now. You should be changing your dressing daily or when it gets wet.";
         } else {
-          response = "I recommend you wait until " + timeOfSurgery.toDateString() + " at " +
+          response = speech = "I recommend you wait until " + timeOfSurgery.toDateString() + " at " +
                      timeOfSurgery.toLocaleTimeString() + " to change your dressing.";
         }
         break;
@@ -64,21 +66,21 @@ exports.heboHttp = function heboHttp (req, res) {
       // Can I shower?
       case "can-i-shower":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-            response = "You can let water run over your " + surgeryArea + ", but you should avoid putting it" +
+            response = speech = "You can let water run over your " + surgeryArea + ", but you should avoid putting it" +
                        " directly in a stream of water. I always recommend a bath over a shower so that you can avoid getting your " + surgeryArea + " wet.";
         } else {
             timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-            response = "Please avoid getting the wound and dressing wet for now. Wait until 48 hours after your surgery, " +
+            response = speech = "Please avoid getting the wound and dressing wet for now. Wait until 48 hours after your surgery, " +
                         timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString() + ". When that time comes, let me know if you need any help!";
         }
         break;
 
       case "do-i-do-the-same-dressing-as-my-doctor":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-            response = "When you change your dressing at home, it will look smaller than the pressure dressing that your doctor used.";
+            response = speech = "When you change your dressing at home, it will look smaller than the pressure dressing that your doctor used.";
         } else {
             timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-            response = "When you change your dressing at home, it will look smaller than the pressure dressing that your doctor used. Be sure to wait until " +
+            response = speech = "When you change your dressing at home, it will look smaller than the pressure dressing that your doctor used. Be sure to wait until " +
                         timeOfSurgery.toDateString() + " at " + timeOfSurgery.toLocaleTimeString() + " to change your dressing.";
         }
         break;
@@ -86,66 +88,83 @@ exports.heboHttp = function heboHttp (req, res) {
       // DIALOG:
       // Do i do the same dressing that doc did?
       // No do normal dressing. How?
-      case "do-i-do-the-same-dressing-as-my-doctor - how":
-        console.log("Show the visual answer of how to change your dressing.");
+      case "do-i-do-the-same-dressing-as-my-doctor - how - bandage":
+        speech = "Here is how you should change your dressing with a bandage."
+        response = "(CHANGE_BANDAGE_RESPONSE)";
+        break;
+
+        case "do-i-do-the-same-dressing-as-my-doctor - how - gauze":
+        speech = "Here is how you should change your dressing with gauze. You will also need non-stick dressing, such as Telfa"
+        response = "(CHANGE_GAUZE_RESPONSE)";
         break;
 
 
       case "dry-blood-stuck-to-dressing":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-            response = "If your bandage is stuck to your wound, try applying a damp cloth to the area to help loosen the bandage. It's often better to have a bandage that's stuck than to pull too hard and potentially cause bleeding.";
+            response = speech = "If your bandage is stuck to your wound, try applying a damp cloth to the area to help loosen the bandage. It's often better to have a bandage that's stuck than to pull too hard and potentially cause bleeding.";
         } else {
             timeOfSurgery.setDate(timeOfSurgery.getDate() + FORTY_EIGHT_HOURS_LATER);
-            response = "I recommend you wait until " + timeOfSurgery.toDateString() + " at " +
+            response = speech = "I recommend you wait until " + timeOfSurgery.toDateString() + " at " +
                        timeOfSurgery.toLocaleTimeString() + " to change your dressing. Once 48 hours has passed, you can use a damp cloth to help loosen your bandage, if it's still stuck.";
         }
         break;
 
       case "help":
         if (upcomingSurgery(timeOfSurgery, currentTime)) {
-          response = "Hi there! I can help you prepare for your surgery! I can answer questions about your surgery that relate to wound care, bleeding, or swelling. You can ask me a question like 'Will I be able to shower?'";
+          response = speech = "Hi there! I can help you prepare for your surgery! I can answer questions about your surgery that relate to wound care, bleeding, or swelling. You can ask me a question like 'Will I be able to shower?'";
         } else {
           var exampleQuestions = ["When can I change my dressing?", "How do I change my dressing?", "Can I shower?", "How do I clean my wound?", "How can I avoid scarring?", "What if my dressing gets wet?"];
-          response = "Hi there! Hope your recovery is going well! I can answer questions about your surgery that relate to wound care, bleeding, or swelling. You can ask me a question like '" + exampleQuestions[Math.floor(Math.random() * exampleQuestions.length)] + "'";
+          response = speech = "Hi there! Hope your recovery is going well! I can answer questions about your surgery that relate to wound care, bleeding, or swelling. You can ask me a question like '" + exampleQuestions[Math.floor(Math.random() * exampleQuestions.length)] + "'";
         }
         break;
 
-      case "how-can-i-stop-my-bleeding - how":
+      case "how-can-i-stop-my-bleeding - how": //TODO: Timing
         // join this together with i am still bleeding and have a variable to track the last time the user said that to see if multiple times in a few hours
-        console.log("visual answer for how to apply pressure");
+        speech = "Here is how you can stop the bleeding."
+        response = "(APPLY_PRESSURE_RESPONSE)";
         break;
 
-      case "how-do-i-change-my-dressing":
-        console.log("visual answer for how to change dressing");
+      case "how-do-i-change-my-dressing - bandage":
+        speech = "Here's how to change your dressing with a bandage.";
+        response = "(CHANGE_BANDAGE_RESPONSE)";
+        break;
+
+        case "how-do-i-change-my-dressing - gauze":
+        speech = "Here's how to change your dressing with gauze. You will also need Telfa or any non-stick dressing.";
+        response = "(CHANGE_GAUZE_RESPONSE)";
         break;
 
       // DIALOG:
       // You should put a lot of vaseline. Would you like me to show you how? Yes
       case "how-do-i-keep-wound-from-drying-out - yes":
-        console.log("visual answer for how to apply vaseline");
+        speech = "Here's how much vaseline you should apply."
+        response = "(VASELINE_RESPONSE)";
         break;
 
       // DIALOG:
       // You should put a lot of vaseline. Would you like me to show you how? Yes
       case "how-much-vaseline - yes":
-        console.log("visual answer for how to apply vaseline");
+        speech = "Here's how much vaseline you should apply. The more the merrier!"
+        response = "(VASELINE_RESPONSE)";
         break;
 
       case "how-should-i-apply-pressure":
-        console.log("VISUAL ANSWER for how to apply pressure");
+        speech = "Here are instructions on how to apply pressure."
+        response = "(APPLY_PRESSURE_RESPONSE)";
         break;
 
       // DIALOG:
       // You should put a lot of vaseline to minimize scarring. Would you like me to show you how? Yes
       case "how-to-reduce-scarring - yes":
-        console.log("visual answer for how to apply vaseline");
+        speech = "This is how much vaseline you should apply."
+        response = "(VASELINE_RESPONSE)";
         break;
 
       case "is-it-ok-to-bleed-through-dressing":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-          response = "It is normal to bleed within the first two weeks. If it has been after two weeks, you should apply pressure to the wound and call your doctor if the bleeding hasn't stopped after 30 minutes."
+          response = speech = "It is normal to bleed within the first two weeks. If it has been after two weeks, you should apply pressure to the wound and call your doctor if the bleeding hasn't stopped after 30 minutes."
         } else {
-          response = "Bleeding is common in the first 48 hours. You should use a dark towel to apply pressure for 30 minutes. This will help stop the bleeding.";
+          response = speech = "Bleeding is common in the first 48 hours. You should use a dark towel to apply pressure for 30 minutes. This will help stop the bleeding.";
         }
         break;
 
@@ -155,9 +174,9 @@ exports.heboHttp = function heboHttp (req, res) {
       // has the bleeding stopped? yes.
       case "What-if-my-dressing-gets-wet? - yes - yes":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-          response = "You should change your dressing and clean the wound area.";
+          response = speech = "You should change your dressing and clean the wound area.";
         } else {
-          response = "Great! It's best to leave your dressing alone. Wait until " + timeOfSurgery.toDateString() + " at " +
+          response = speech = "Great! It's best to leave your dressing alone. Wait until " + timeOfSurgery.toDateString() + " at " +
                      timeOfSurgery.toLocaleTimeString() + " to change your dressing.";
         }
         break;
@@ -167,39 +186,39 @@ exports.heboHttp = function heboHttp (req, res) {
       // Is it wet from blood? no.
       case "What-if-my-dressing-gets-wet? - no":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-          response = "You should replace your dressing whenever it gets wet.";
+          response = speech = "You should replace your dressing whenever it gets wet.";
         } else {
-          response = "It's best to leave your dressing alone. For now you can try to pat it dry. Wait until " + timeOfSurgery.toDateString() + " at " +
+          response = speech = "It's best to leave your dressing alone. For now you can try to pat it dry. Wait until " + timeOfSurgery.toDateString() + " at " +
                      timeOfSurgery.toLocaleTimeString() + " to change your dressing.";
         }
         break;
 
       case "what-should-i-do-if-the-dressing-is-falling-off":
         if (surgeryArea === "forehead" || surgeryArea === "scalp" || surgeryArea === "back of neck") {
-            response = "I see that your surgery was on your " + surgeryArea + ". If you have hair, it might be getting in the way " +
+            response = speech = "I see that your surgery was on your " + surgeryArea + ". If you have hair, it might be getting in the way " +
                         "of the bandage. Try to secure the tape on the skin under your hair. You can also try wearing a hat " +
                         "or a scarf to keep the dressing in place.";
         } else {
-            response = "You can try adding more tape if your dressing is coming loose.";
+            response = speech = "You can try adding more tape if your dressing is coming loose.";
         }
         break;
 
       case "when-can-i-change-dressing":
         if (isAfterFortyEightHours(timeOfSurgery, currentTime)) {
-          response = "You can change your dressing now. You should be changing your dressing daily or when it gets wet.";
+          response = speech = "You can change your dressing now. You should be changing your dressing daily or when it gets wet.";
         } else {
-          response = "You can change your dressing on" + timeOfSurgery.toDateString() + " at " +
+          response = speech = "You can change your dressing on" + timeOfSurgery.toDateString() + " at " +
                      timeOfSurgery.toLocaleTimeString() + ".";
         }
         break;
 
       default:
-        response = "I'm sorry, I don't think I know the answer to that question. I can only answer questions on bleeding, swelling, and wound care."
+        response = speech = "I'm sorry, I don't think I know the answer to that question. I can only answer questions on bleeding, swelling, and wound care."
     }
 
 
     res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-    res.send(JSON.stringify({ "speech": response, "displayText": response
+    res.send(JSON.stringify({ "speech": speech, "displayText": response
     //"speech" is the spoken version of the response, "displayText" is the visual version
     }));
 };
